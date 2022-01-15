ec8313ed49121925a05ca0f5a854c016978ea63a
enockabere <maebaenock95@gmail.com>
2022-01-16 00:15:08 +0300

    deployment

--- post/admin.py
+++ post/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from . models import Catalogue, Product
+from . models import Catalogue
 
 
 # Register your models here.
 admin.site.register(Catalogue)
-admin.site.register(Product)

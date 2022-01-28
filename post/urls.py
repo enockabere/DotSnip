7d334b1a1e14a662958cdbf35c1921f3ebf0f6a5
enockabere <maebaenock95@gmail.com>
2022-01-28 12:43:30 +0300

    deployment

--- post/urls.py
+++ post/urls.py
@@ -1,8 +1,13 @@
 from  django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("post", views.post,name="post"),
     path("product/<int:pk>/", views.product,name="product"),
     path("entry/<int:pk>/", views.Entry,name="entry"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
